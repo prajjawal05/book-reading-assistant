ae44d835162e2d507b1684f020ab68fe525121ea
prajjawal05 <prajjawalagarwal@gmail.com>
2022-10-27 02:14:52 -0400

    trying kivy for build

--- ui.py
+++ ui.py
@@ -1,12 +1,20 @@
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 
-app = QApplication([])
+from speech import synthesize_from_mic
 
-window = QWidget()
-window.setWindowTitle("PyQt App")
-window.setGeometry(100, 100, 280, 80)
-helloMsg = QLabel("<h1>Hello, World!</h1>", parent=window)
-helloMsg.move(60, 15)
+class MyLayout(BoxLayout):
 
-window.show()
-app.exec()
+    def __init__(self, **kwargs):
+        super(MyLayout, self).__init__(**kwargs)
+
+    def generate_number(self):
+        synthesize_from_mic()
+
+class Window(App):
+    def build(self):
+        return MyLayout()
+
+
+neural_random  = Window()
+neural_random.run()
